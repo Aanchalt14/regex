2cec6486c02f0a8ec6e852b5ad947ba9c2bcf058
Aanchalt14 <aanchalt71@gmail.com>
2020-11-28 11:43:17 +0530

    Added : regex last name

---
+++ Lastname_Validation.sh
@@ -0,0 +1,12 @@
+function Lastname()
+{
+  echo " Enter first name " name
+  Lastname="^[A-Z]{1}+[a-z A-Z]{2,}$"
+  
+  if[ Lastname==name ]
+  then
+  echo " Valid name "
+  else
+  echo "Invalid name "
+  fi
+}
