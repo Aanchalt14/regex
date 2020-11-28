65e042b84d978e1a3975bfb7d30933c72ee4312e
Aanchalt14 <aanchalt71@gmail.com>
2020-11-28 13:37:10 +0530

    Added : Email Sample Validation

---
+++ EmailSample_Validation.sh
@@ -0,0 +1,12 @@
+function EmailS()
+{
+  echo " Enter email " ename
+  email1="^[A-Z]{1}+[a-z A-Z]{2,}$"
+  
+  if[ email1==ename ]
+  then
+  echo " Valid email "
+  else
+  echo "Invalid email "
+  fi
+}
