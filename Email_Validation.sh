cf7a39f509d3f5c343b01c059f1446ca9db2d1cb
Aanchalt14 <aanchalt71@gmail.com>
2020-11-28 12:58:52 +0530

    Added : regex email

---
+++ Email_Validation.sh
@@ -0,0 +1,12 @@
+function Email()
+{
+  echo " Enter Email " Ename
+  Email="^[a-z A-Z 0-9]+@[.\-_+ 0-9 a-z A-Z]*$"
+  
+  if[ Email==Ename ]
+  then
+  echo " Valid email "
+  else
+  echo "Invalid email "
+  fi
+}
