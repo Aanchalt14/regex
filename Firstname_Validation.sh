22368bac9bb21890312aaca8a5f724e53a55e97c
Aanchalt14 <aanchalt71@gmail.com>
2020-11-28 11:35:31 +0530

    Added : regex first name

---
+++ Firstname_Validation.sh
@@ -0,0 +1,12 @@
+function Firstname()
+{
+  echo " Enter first name " name
+  firstname="^[A-Z]{1}+[a-z A-Z]{2,}$"
+  
+  if[ firstname==name ]
+  then
+  echo " Valid name "
+  else
+  echo "Invalid name "
+  fi
+}
